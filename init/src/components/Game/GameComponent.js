be568c2f5b20e83bad5128224ce993f46acb6713
ZlowrogiTuru <bartela.piotr+bitbucket@gmail.com>
2017-08-24 16:08:50 +0200

    remove project and create new with create react app

--- init/src/components/Game/GameComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const GameComponent = props => {
-  return <div className='game-color'> Game on! </div>;
-};
-
-export default GameComponent;
--- init/src/components/Game/index.js
+++
@@ -1,3 +0,0 @@
-import GameComponent from './GameComponent';
-
-export { GameComponent };
